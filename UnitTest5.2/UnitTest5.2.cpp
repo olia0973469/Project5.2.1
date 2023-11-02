f61197fecb7f4e9971922638debbc72f705578de
olia0973469 <Olha.Tykha.RI.2023@lpnu.ua>
2023-11-02 12:33:37 +0200

    Add project files.

---
+++ UnitTest5.2/UnitTest5.2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Project5.2/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest52
+{
+	TEST_CLASS(UnitTest52)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = A(1, 2, 3);
+			Assert::AreEqual(t, -1.8, 0.0000001);
+		}
+	};
+}
